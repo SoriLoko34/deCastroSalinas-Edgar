271a49ac829f9611082965e70b70b736515308e8
SoriLoko34 <edgardecastro.2007@gmail.com>
2025-10-23 08:55:05 +0200

    Update HolaMundo.java

--- deCastroSalinas-Edgar/HolaMundo.java
+++ deCastroSalinas-Edgar/HolaMundo.java
@@ -1,7 +1,8 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hola Mundo!!!");
     }
 }
 
 // Autor: Edgar de Castro Salinas
+
